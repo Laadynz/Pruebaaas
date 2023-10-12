5687f618d4bf5f433c2c1bbb6c478bc96247fc57
Sebastian Gpe Tanori Ruiz <tanoriruizs@gmail.com>
2023-10-11 19:41:26 -0700

    Agregando Clientes

---
+++ Pruebaaas/Shared/Models/ProductoAgregarDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Pruebaaas.Shared.Models
+{
+    public class ProductoAgregarDto
+    {
+        public string Nombre { get; set; }
+        public decimal UnidadesEnStock { get; set; }
+        public int ClaveProducto { get; set; }
+        public int ClasificacionId { get; set; }
+    }
+}
---
+++ Pruebaaas/Shared/Models/ProductoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Pruebaaas.Shared.Models
+{
+    public class ProductoDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal UnidadesEnStock { get; set; }
+        public int ClaveProducto { get; set; }
+        public int ClasificacionId { get; set; }
+    }
+}
